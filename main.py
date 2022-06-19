549a67dbfc22b9f61fdf8c98eb782f89b45c6d2d
lukasdext <lukasdext160@hotmail.com>
2022-06-19 17:25:46 -0300

    subindo o projeto

---
+++ main.py
@@ -0,0 +1,29 @@
+# importanto
+import os 
+import time
+import smtplib
+from email.message import EmailMessage
+
+# importanto variavel 
+from conta import senha 
+from conta import mail 
+
+# Configuração de email e senha
+EMAIL_ADDRESS = mail
+EMAIL_PASSWORD = senha
+
+# Criando e-mail
+
+msg = EmailMessage()
+
+# Titulo do email
+msg ['Subject'] = 'Mensagem de titulo'
+msg ['From'] = 'projetoking160@gmail.com'
+msg['To'] = 'lukasdext160@hotmail.com'
+msg.set_content('Mensagem dentro do email')
+
+ # Enviar um e-mail
+with smtplib.SMTP_SSL('smtp.gmail.com',443) as smtp:
+    smtp.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
+    smtp.send_message(msg)
+
